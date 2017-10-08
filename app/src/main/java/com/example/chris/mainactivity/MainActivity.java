032f21675225027315483eb8c8a98d9086578b89
Christian Gerard Hizon <31374181+ChirstianHizon@users.noreply.github.com>
2017-10-08 14:16:56 +0800

    PayMaya Integration

--- app/src/main/java/com/example/chris/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/chris/mainactivity/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.chris.mainactivity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.chris.mainactivity.PayMaya.PaymayalSample;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Activity constant =  this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(constant,PaymayalSample.class));
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/chris/mainactivity/Objects/Constants.java
@@ -0,0 +1,12 @@
+package com.example.chris.mainactivity.Objects;
+
+/**
+ * Created by chris on 08/10/2017.
+ */
+
+public class Constants {
+
+    public static String PayMayaKey = "pk-N6TvoB4GP2kIgNz4OCchCTKYvY5kPQd2HDRSg8rPeQG";
+
+    public static String PaypalKey = "";
+}
