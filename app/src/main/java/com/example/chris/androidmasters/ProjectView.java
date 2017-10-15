9a42cf8ada098e2e7e225b1576b2454a60e92474
Christian Gerard Hizon <31374181+ChirstianHizon@users.noreply.github.com>
2017-10-16 00:02:24 +0800

    Added Progress

--- app/src/main/java/com/example/chris/androidmasters/ProjectView.java
+++ app/src/main/java/com/example/chris/androidmasters/ProjectView.java
@@ -2,7 +2,11 @@ package com.example.chris.androidmasters;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -11,6 +15,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.chris.androidmasters.Objects.Details;
@@ -20,6 +25,8 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.squareup.picasso.Picasso;
+
+import static com.example.chris.androidmasters.Objects.Constants.getCurrencySymbol;
 
 public class ProjectView extends AppCompatActivity {
 
@@ -67,7 +74,9 @@ public class ProjectView extends AppCompatActivity {
         });
 
         db = FirebaseFirestore.getInstance();
+        
         getProjectDetails();
+        getProjectProgress();
     }
 
     @Override
@@ -156,6 +165,57 @@ public class ProjectView extends AppCompatActivity {
                         .into(myImage);
             }
         }
+
+    }
+
+    private void getProjectProgress(){
+
+        final ProgressBar pbprogress = (ProgressBar) findViewById(R.id.pb_progress);
+        final TextView tvcurrent = (TextView) findViewById(R.id.tv_current);
+        final TextView tvgoal = (TextView) findViewById(R.id.tv_goal);
+
+        DocumentReference docRef = db.collection("Projects").document(id);
+        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.w(TAG, "Listen failed.", e);
+                    return;
+                }
+
+                if (snapshot != null && snapshot.exists()) {
+                    Log.d(TAG, "Current data: " + snapshot.getData());
+
+                    Double goal = Double.valueOf(snapshot.getString("goal"));
+                    Double current = Double.valueOf(snapshot.getString("current"));
+
+                    Double progress = Double.valueOf(snapshot.getString("current")) / Double.valueOf(snapshot.getString("goal")) * 100;
+
+                    String currency = getCurrencySymbol("PHP");
+                    tvgoal.setText(currency+" "+snapshot.getString("goal"));
+                    tvcurrent.setText(currency+" "+snapshot.getString("current"));
+
+                    if(current > goal){
+                        pbprogress.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+                    }else if(current.equals(goal)){
+                        pbprogress.getProgressDrawable().setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_IN);
+                    }else{
+                        pbprogress.getProgressDrawable().setColorFilter(Color.GREEN, PorterDuff.Mode.SRC_IN);
+                    }
+
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        pbprogress.setProgress(progress.intValue(),true);
+                    }else{
+                        pbprogress.setProgress(progress.intValue());
+                    }
+
+                } else {
+                    Log.d(TAG, "Current data: null");
+                }
+            }
+        });
 
     }
 
