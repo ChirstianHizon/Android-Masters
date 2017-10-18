240b151503d533c99613fca0d5677d522296ff2f
teamboob <teamboob@github.com>
2017-10-19 01:50:49 +0800

    incomplete logo

--- app/src/main/java/com/example/chris/androidmasters/SplashActivity.java
+++ app/src/main/java/com/example/chris/androidmasters/SplashActivity.java
@@ -2,10 +2,13 @@ package com.example.chris.androidmasters;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -19,12 +22,18 @@ public class SplashActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
+        ImageView imgLogo = (ImageView)findViewById(R.id.img_logo);
+
+        AnimatedVectorDrawable d = (AnimatedVectorDrawable) getDrawable(R.drawable.vector_logo_anim); // Insert your AnimatedVectorDrawable resource identifier
+        imgLogo.setImageDrawable(d);
+        d.start();
+
         Thread splashThread = new Thread(){
             @Override
             public void run(){
                 try{
                     int waited = 0;
-                    while(waited < 2500){
+                    while(waited < 30000){
                         sleep(100);
                         waited += 100;
                     }
