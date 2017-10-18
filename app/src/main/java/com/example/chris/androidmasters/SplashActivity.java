2e0dede23d1cb84e1165da14b9104a5853ae8ae5
teamboob <teamboob@github.com>
2017-10-18 16:27:42 +0800

    tian 4 u

--- app/src/main/java/com/example/chris/androidmasters/SplashActivity.java
+++ app/src/main/java/com/example/chris/androidmasters/SplashActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,7 +14,30 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        Thread splashThread = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    int waited = 0;
+                    while(waited < 4500){
+                        sleep(100);
+                        waited += 100;
+                    }
+                }catch(InterruptedException e){
+                    //do nothing
+                }finally{
+                    finish();
+                    startActivity(new Intent(constant,ProjectListActivity.class));
+                }
+            }
+        };
+
+        splashThread.start();
 
         //Splash Page
 
