4ee10b2c13abde3a6cd08bd65b26f5fad97a5452
Christian Gerard Hizon <31374181+ChirstianHizon@users.noreply.github.com>
2017-10-19 23:35:38 +0800

    Fixed Intro and Splash

--- app/src/main/java/com/example/chris/androidmasters/Introduction/FirstSlide.java
+++ app/src/main/java/com/example/chris/androidmasters/Introduction/FirstSlide.java
@@ -2,31 +2,44 @@ package com.example.chris.androidmasters.Introduction;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.chris.androidmasters.IntroActivity;
 import com.example.chris.androidmasters.R;
+import com.github.paolorotolo.appintro.ISlideBackgroundColorHolder;
 
 /**
  * Created by chris on 19/10/2017.
  */
 
-public class FirstSlide extends Fragment {
+public class FirstSlide extends Fragment implements ISlideBackgroundColorHolder {
+
+    private View v;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.intro_slide1, container, false);
 
-        IntroActivity activity =(IntroActivity)getActivity();
+        v = inflater.inflate(R.layout.intro_slide1, container, false);
 
-        activity.setBarColor(Color.parseColor("#FFFFFF"));
-        activity.setSeparatorColor(Color.parseColor("#FFFFFF"));
-
-
-
+//        IntroActivity activity =(IntroActivity)getActivity();
+//        activity.setBarColor(Color.parseColor("#FFFFFF"));
+//        activity.setSeparatorColor(Color.parseColor("#FFFFFF"));
         return v;
+    }
+
+    @Override
+    public int getDefaultBackgroundColor() {
+        return Color.parseColor("#FFFFFF");
+    }
+
+    @Override
+    public void setBackgroundColor(@ColorInt int backgroundColor) {
+
+        if (v != null) {
+            v.setBackgroundColor(backgroundColor);
+        }
     }
 }
--- app/src/main/java/com/example/chris/androidmasters/Introduction/SecondSlide.java
+++ app/src/main/java/com/example/chris/androidmasters/Introduction/SecondSlide.java
@@ -2,29 +2,44 @@ package com.example.chris.androidmasters.Introduction;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.chris.androidmasters.IntroActivity;
 import com.example.chris.androidmasters.R;
+import com.github.paolorotolo.appintro.ISlideBackgroundColorHolder;
 
 /**
  * Created by Lenovo on 19/10/2017.
  */
 
-public class SecondSlide extends Fragment {
+public class SecondSlide extends Fragment implements ISlideBackgroundColorHolder {
+    View v;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.intro_slide2, container, false);
 
-        IntroActivity activity =(IntroActivity)getActivity();
+        v = inflater.inflate(R.layout.intro_slide2, container, false);
 
-        activity.setBarColor(Color.parseColor("#2494a2"));
-        activity.setSeparatorColor(Color.parseColor("#2494a2"));
+//        IntroActivity activity =(IntroActivity)getActivity();
+//        activity.setBarColor(Color.parseColor("#2494a2"));
+//        activity.setSeparatorColor(Color.parseColor("#2494a2"));
 
         return v;
+    }
+
+    @Override
+    public int getDefaultBackgroundColor() {
+        return Color.parseColor("#2494a2");
+    }
+
+    @Override
+    public void setBackgroundColor(@ColorInt int backgroundColor) {
+
+        if (v != null) {
+            v.setBackgroundColor(backgroundColor);
+        }
     }
 }
