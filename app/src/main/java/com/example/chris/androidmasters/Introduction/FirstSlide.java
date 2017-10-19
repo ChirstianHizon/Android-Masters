1789a54d8daa0a79c0612297db0a1b99bfdb1663
teamboob <teamboob@github.com>
2017-10-19 23:56:42 +0800

    slides pt 4

--- app/src/main/java/com/example/chris/androidmasters/Introduction/FirstSlide.java
+++ app/src/main/java/com/example/chris/androidmasters/Introduction/FirstSlide.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.chris.androidmasters.IntroActivity;
 import com.example.chris.androidmasters.R;
 import com.github.paolorotolo.appintro.ISlideBackgroundColorHolder;
 
@@ -24,16 +25,13 @@ public class FirstSlide extends Fragment implements ISlideBackgroundColorHolder 
 
         v = inflater.inflate(R.layout.intro_slide1, container, false);
 
-<<<<<<< HEAD
-        activity.setBarColor(Color.parseColor("#56bda2"));
-        activity.setSeparatorColor(Color.parseColor("#56bda2"));
-=======
+        IntroActivity activity =(IntroActivity)getActivity();
+
 //        IntroActivity activity =(IntroActivity)getActivity();
 //        activity.setBarColor(Color.parseColor("#FFFFFF"));
 //        activity.setSeparatorColor(Color.parseColor("#FFFFFF"));
         return v;
     }
->>>>>>> dbbc708ce4e125afa8e3b13a1d38fbf8f71f53dd
 
     @Override
     public int getDefaultBackgroundColor() {
